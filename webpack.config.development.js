dbaf3695d45271d25c9bb5ed860036cdd16368c7
Aries <urchinzhou@gmail.com>
2024-03-13 18:28:45 +0800

    add code split by route and vendor

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 3000;
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    vendor: ["semantic-ui-react"],
+    app: "./src/index.js",
+  },
   output: {
-    filename: "bundle.[hash].js",
+    filename: "[name].[hash].js",
     publicPath: "/",
   },
   resolve: {
@@ -57,5 +60,23 @@ module.exports = {
     historyApiFallback: true,
     open: true,
     hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+        vendor: {
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true,
+        },
+      },
+    },
   },
 };
